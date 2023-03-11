daaaa35cb5baf001b3609bfe8cd5fbe8c22100ae
쇼쇼라 <cshnv1121@naver.com>
2023-03-11 15:24:49 +0900

    test

--- sulsul_Project/src/main/java/com/sist/dao/NoticeDAO.java
+++ sulsul_Project/src/main/java/com/sist/dao/NoticeDAO.java
@@ -2,7 +2,11 @@ package com.sist.dao;
 
 import java.util.Map;
 import java.util.*;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.sist.vo.*;
@@ -25,4 +29,49 @@ public class NoticeDAO {
 	 {
 		 return mapper.noticeTotalPage();
 	 }
+	 
+//	 @Insert("INSERT INTO sul_notice_2_2 VALUES("
+//	         + "#{no},#{name},#{subject},#{content},#{pwd},SYSDATE,0)")
+	   public void noticeInsert(NoticeVO vo)
+	   {
+		   mapper.noticeInsert(vo);
+	   }
+	   
+//	   @Select("SELECT no,name,subject,content,TO_CHAR(regdate, 'YYYY-MM-DD') as dbday,hit "
+//	         +"FROM sul_notice_2_2 "
+//	         +"WHERE no=#{no}")
+	   public NoticeVO noticeDetailData(int no)
+	   {
+		   mapper.noticeHitIncrement(no);
+		   return mapper.noticeDetailData(no);
+	   }
+//		   @Update("UPDATE sul_notice_2_2 SET "
+//		         +"subject=#{subject},content=#{content} "
+//		         +"WHERE no=#{no}")
+		   public String noticeUpdate(NoticeVO vo)
+		   {
+				String res="";
+				String db_pwd=mapper.noticeGetPassword(vo.getNo());
+				if(db_pwd.equals(vo.getPwd()))
+				{
+					mapper.noticeUpdate(vo);
+					res="yes";
+				}
+			     return res;
+		   }
+		   
+//		   @Delete("DELETE FROM sul_notice_2_2 "
+//		         +"WHERE no=#{no}")
+		   public String noticeDelete(int no,String pwd)
+		   { 
+			   String res="no";
+		      String db_pwd=mapper.noticeGetPassword(no);
+		      if(db_pwd.equals(pwd))
+		      {
+		         mapper.noticeDelete(no);
+		         res="yes";
+		      
+		     }
+		       return res;
+		   }
 }
